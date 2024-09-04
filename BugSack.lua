178fa8a64238dd9cf8fdd40829fb2bf7d521b1ea
wanghaiji <wanghaiji@163.com>
2024-09-04 11:12:59 +0800

    1

---
+++ BugSack.lua
@@ -0,0 +1,13 @@
+
+BugSackDB = {
+["auto"] = false,
+["fontSize"] = "GameFontHighlight",
+["useMaster"] = false,
+["altwipe"] = true,
+["mute"] = false,
+["soundMedia"] = "BugSack: Fatality",
+["chatframe"] = false,
+}
+BugSackLDBIconDB = {
+["minimapPos"] = 115.7565424663447,
+}
---
+++ PremadeGroupsFilter.lua
@@ -0,0 +1,17 @@
+
+PremadeGroupsFilterSettings = {
+["signupOnEnter"] = false,
+["coloredGroupTexts"] = true,
+["ratingInfo"] = true,
+["specIcon"] = false,
+["oneClickSignUp"] = true,
+["dialogMovable"] = true,
+["leaderCrown"] = false,
+["missingRoles"] = false,
+["version"] = 3,
+["classBar"] = false,
+["classCircle"] = false,
+["persistSignUpNote"] = true,
+["skipSignUpDialog"] = false,
+["classNamesInTooltip"] = true,
+}
