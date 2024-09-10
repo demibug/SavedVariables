239bc14b9fdf6a231e6ad2b530a7d68dd32dfc4a
wanghaiji <wanghaiji@163.com>
2024-09-10 13:59:15 +0800

    1

--- !BugGrabber.lua
+++ !BugGrabber.lua
@@ -1,7 +1,7 @@
 
 BugGrabberDB = {
-["session"] = 112,
 ["lastSanitation"] = 3,
+["session"] = 113,
 ["errors"] = {
 {
 ["message"] = "Lua 錯誤，在 WA 提醒效果 'DZKT_6_骰子': 觸發器 6\nWeakAuras 版本: 5.17.0\n堆疊層數追蹤:\n[string \"return function()\"]:12: attempt to call global 'UnitBuff' (a nil value)",
@@ -20,20 +20,20 @@ BugGrabberDB = {
 ["counter"] = 1,
 },
 {
-["message"] = "Interface/AddOns/BtWQuests/BtWQuestsCharacters.lua:911: attempt to index local 'factionData' (a nil value)",
-["time"] = "2024/09/08 21:56:16",
-["locals"] = "tbl = <table> {\n 2590 = <table> {\n }\n 2569 = <table> {\n }\n 2570 = <table> {\n }\n 2594 = <table> {\n }\n}\ntbl = <table> {\n 2590 = <table> {\n }\n 2569 = <table> {\n }\n 2570 = <table> {\n }\n 2594 = <table> {\n }\n}\nnumEntries = 12\n(for index) = 6\n(for limit) = 12\n(for step) = 1\ni = 6\nfactionData = nil\n(*temporary) = nil\n(*temporary) = 6\n(*temporary) = nil\n(*temporary) = \"attempt to index local 'factionData' (a nil value)\"\ntemp = <table> {\n 2506 = <table> {\n }\n 2590 = <table> {\n }\n 1168 = <table> {\n }\n 2569 = <table> {\n }\n 2600 = <table> {\n }\n 2594 = <table> {\n }\n 1169 = <table> {\n }\n 2640 = <table> {\n }\n 1118 = <table> {\n }\n 2570 = <table> {\n }\n}\n",
-["stack"] = "[string \"@Interface/AddOns/BtWQuests/BtWQuestsCharacters.lua\"]:911: in function <Interface/AddOns/BtWQuests/BtWQuestsCharacters.lua:899>\n[string \"@Interface/AddOns/BtWQuests/BtWQuestsCharacters.lua\"]:1114: in function `OnEvent'\n[string \"@Interface/AddOns/BtWQuests/BtWQuestsCharacters.lua\"]:1191: in function <Interface/AddOns/BtWQuests/BtWQuestsCharacters.lua:1190>",
-["session"] = 102,
-["counter"] = 2,
-},
-{
 ["message"] = "Interface/AddOns/Details/functions/ej_cache.lua:47: attempt to index field 'CacheEncountersBy_EncounterId' (a nil value)",
 ["time"] = "2024/09/09 23:21:20",
 ["locals"] = "Skipped (In Encounter)",
 ["stack"] = "[string \"@Interface/AddOns/Details/functions/ej_cache.lua\"]:47: in function `GetEncounterInfo'\n[string \"@Interface/AddOns/Details/core/control.lua\"]:156: in function <Interface/AddOns/Details/core/control.lua:134>\n[string \"@Interface/AddOns/Details/core/control.lua\"]:396: in function `StartCombat'\n[string \"@Interface/AddOns/Details/core/parser.lua\"]:955: in function <Interface/AddOns/Details/core/parser.lua:752>\n[string \"=(tail call)\"]: ?",
 ["session"] = 111,
 ["counter"] = 1,
+},
+{
+["message"] = "Interface/AddOns/BtWQuests/BtWQuestsCharacters.lua:911: attempt to index local 'factionData' (a nil value)",
+["time"] = "2024/09/08 21:56:16",
+["locals"] = "tbl = <table> {\n 2590 = <table> {\n }\n 2569 = <table> {\n }\n 2570 = <table> {\n }\n 2594 = <table> {\n }\n}\ntbl = <table> {\n 2590 = <table> {\n }\n 2569 = <table> {\n }\n 2570 = <table> {\n }\n 2594 = <table> {\n }\n}\nnumEntries = 12\n(for index) = 6\n(for limit) = 12\n(for step) = 1\ni = 6\nfactionData = nil\n(*temporary) = nil\n(*temporary) = 6\n(*temporary) = nil\n(*temporary) = \"attempt to index local 'factionData' (a nil value)\"\ntemp = <table> {\n 2506 = <table> {\n }\n 2590 = <table> {\n }\n 1168 = <table> {\n }\n 2569 = <table> {\n }\n 2600 = <table> {\n }\n 2594 = <table> {\n }\n 1169 = <table> {\n }\n 2640 = <table> {\n }\n 1118 = <table> {\n }\n 2570 = <table> {\n }\n}\n",
+["stack"] = "[string \"@Interface/AddOns/BtWQuests/BtWQuestsCharacters.lua\"]:911: in function <Interface/AddOns/BtWQuests/BtWQuestsCharacters.lua:899>\n[string \"@Interface/AddOns/BtWQuests/BtWQuestsCharacters.lua\"]:1114: in function `OnEvent'\n[string \"@Interface/AddOns/BtWQuests/BtWQuestsCharacters.lua\"]:1191: in function <Interface/AddOns/BtWQuests/BtWQuestsCharacters.lua:1190>",
+["session"] = 113,
+["counter"] = 3,
 },
 },
 }
